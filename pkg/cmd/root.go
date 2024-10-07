43786d286a2c083fecd192e22eef98c2991988f9
Patrick Foh <patrickfoh@gmail.com>
2024-10-07 16:36:01 +0100

    Bump to 0.1.1

--- pkg/cmd/root.go
+++ pkg/cmd/root.go
@@ -14,7 +14,7 @@ var (
 	NamecheapClientIP string
 	NamecheapSandbox  bool
 	OutputFormat      string
-	Version           = "0.1.0"
+	Version           = "0.1.1"
 )
 
 // NewBaseCommand creates a new base command
